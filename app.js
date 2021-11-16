294fa92e66d1e8c85c4c3713161160a3a426b145
Ariel Dorssi <adorssi@gmail.com>
2021-11-15 23:23:32 -0300

    Se agregan formularios registro y login

--- app.js
+++ app.js
@@ -6,8 +6,14 @@ app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'views/home.html'));
-})
+});
 
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/login.html'));
+});
 
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/register.html'));
+});
 
 app.listen(3000);
